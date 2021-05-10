6c27d4927fb55d54d9c6b289c99dc7bb42e38a3c
Yan Feng <fengyan0121@gmail.com>
2021-05-10 12:12:23 -0400

    Remove App.js initial content

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route, Switch, withRouter, Redirect} from 'react-router-dom';
+
+const App = props => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default App;
